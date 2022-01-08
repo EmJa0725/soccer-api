695025dbd450b3c87b37abe4463c9bbfab79bea5
Julian Andres Mendez Romero <67122190+EmJa0725@users.noreply.github.com>
2022-01-08 11:35:56 -0500

    Responsive conflicts fixed

--- src/components/MyNavbar.js
+++ src/components/MyNavbar.js
@@ -3,16 +3,24 @@ import { NavLink } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import navbarIcon from "../assets/img/main/navbar-icon.png";
+import navbarIcon from "../assets/img/navbar/navbar-icon.png";
 import { leagues } from '../config/leaguesDictionary';
 import { useNavigate } from 'react-router-dom';
+import { Collapse } from 'bootstrap';
 
 
 const MyNavbar = () => {
     const navigate = useNavigate();
 
+    const collapseToogle  = () => {
+        let myCollapse = document.getElementById('navbar-items')
+        new Collapse(myCollapse, {
+            show: false
+        })
+    }
+
     return (
-        <Navbar bg="dark" variant="dark" expand="lg" className="pt-xs-0 pt-md-0 pt-xs-0 pb-md-0 fixed-top">
+        <Navbar bg="dark" variant="dark" expand="lg" className="pt-xs-0 pt-md-0 pt-xs-0 pb-md-0 fixed-top" id="main-navbar">
             <Container>
                 <Navbar.Brand className="navbar-brand" onClick={() => navigate("/")}>
                     <img
@@ -20,7 +28,7 @@ const MyNavbar = () => {
                         src={navbarIcon}
                         width="40"
                         height="40"
-                        className="d-inline-block align-top me-4"
+                        className="d-inline-block align-top me-3 me-sm-4"
                     />
                     <span className="d-inline-block align-middle navbar-title">Football leagues standings</span>
                 </Navbar.Brand>
@@ -30,8 +38,10 @@ const MyNavbar = () => {
                         {leagues.map((league, idx) => (
                             <NavLink
                                 key={idx}
+                                onClick={() => collapseToogle()}
                                 className={({ isActive }) => (isActive ? 'nav-link selected' : 'nav-link')}
-                                to={`standings/${league.name}`} state={{ data: { ...league } }}>
+                                to={`standings/${league.name}`} state={{ data: { ...league } }}
+                                >
                                 <img className="navbar-image" src={league.headerLogo} alt={league.name} />
                             </NavLink>
                         ))}
